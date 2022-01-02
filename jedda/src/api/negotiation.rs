73afbd0ecf80982ee29764b9e1193ceaa89ae390
Chris Allen <cma@bitemyapp.com>
2022-01-01 23:20:29 -0600

    Error trait sugar, cleanup

--- jedda/src/api/negotiation.rs
+++ jedda/src/api/negotiation.rs
@@ -1,13 +1,23 @@
 use rcgen;
 
+use tracing::trace;
 use crate::error;
+use crate::error::IntoJeddaError;
 
-const PROTOCOL_VERSION: u8 = 1;
+pub const PROTOCOL_VERSION: u8 = 1;
 
-// Usually PROTOCOL_VERSION
-async fn connection_info(writer: &mut impl std::io::Write, protocol_version: u8, hostname: &str, port: u32, server_cert: rcgen::Certificate) -> error::Result<()> {
-    writeln!(
-        writer,
+#[tracing::instrument]
+pub fn generate_cert() -> Result<rcgen::Certificate, rcgen::RcgenError> {
+    let subject_alt_names =
+        vec!["localhost".to_string()];
+
+    let cert = rcgen::generate_simple_self_signed(subject_alt_names)?;
+    trace!("\n{}\n", cert.serialize_pem()?);
+    Ok(cert)
+}
+
+pub fn connection_info(protocol_version: u8, hostname: &str, port: u32, server_cert: rcgen::Certificate) -> String {
+    format!(
         // "localhost:10000"
         "{}|5|tcp|{}:{}|grpc|{}",
         protocol_version,
@@ -17,18 +27,30 @@ async fn connection_info(writer: &mut impl std::io::Write, protocol_version: u8,
             server_cert.serialize_der().unwrap(),
             base64::STANDARD_NO_PAD
         )
-    ).map_err(|_| error::Error::ConnectionNegotiation("Failed to write connection information to Writer".to_string()))?;
+    )
+}
+
+pub fn write_connection_info(writer: &mut impl std::io::Write, protocol_version: u8, hostname: &str, port: u32, server_cert: rcgen::Certificate) -> error::Result<()> {
+    let info_line = connection_info(protocol_version, hostname, port, server_cert);
+    writeln!(
+        writer,
+        // "localhost:10000"
+        "{}",
+        info_line,
+    ).negotiation_error("Failed to write connection information to Writer")?;
     Ok(())
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::api::negotiation::{connection_info, PROTOCOL_VERSION};
+    use test_log::test;
+    use crate::api::negotiation::{generate_cert, PROTOCOL_VERSION, write_connection_info};
 
-    #[tokio::test]
-    async fn outputs_connection_info_to_stdio() {
+    #[test(tokio::test)]
+    async fn outputs_connection_info_to_stdio() -> Result<(), Box<dyn std::error::Error>> {
         let mut stdout = std::io::stdout();
-        connection_info(&mut stdout, PROTOCOL_VERSION, "localhost", 12000, todo!()).await.unwrap();
+        let cert = generate_cert().unwrap();
+        write_connection_info(&mut stdout, PROTOCOL_VERSION, "localhost", 12000, cert).unwrap();
+        Ok(())
     }
 }
--- jedda/src/error.rs
+++ jedda/src/error.rs
@@ -1,6 +1,38 @@
+use tracing::error;
+
 #[derive(Debug)]
 pub enum Error {
     ConnectionNegotiation(String),
 }
 
 pub type Result<V> = core::result::Result<V, Error>;
+
+pub trait IntoJeddaError<T> {
+    fn jedda_error(
+        self,
+        error_val: Error,
+    ) -> Result<T>;
+
+    fn negotiation_error<S>(self, message: S) -> Result<T>
+        where
+            S: AsRef<str>,
+            Self: std::marker::Sized,
+    {
+        self.jedda_error(Error::ConnectionNegotiation(message.to_string()))
+    }
+}
+
+impl<T, E: std::fmt::Debug> IntoJeddaError<T> for core::result::Result<T, E> {
+    fn jedda_error(
+        self,
+        error_val: Error
+    ) -> core::result::Result<T, actix_web::Error> {
+        match self {
+            Ok(val) => Ok(val),
+            Err(err) => {
+                error!("http_error: {:?}", err);
+                Err(error_val)
+            }
+        }
+    }
+}
